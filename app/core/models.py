c75c7675364acd8dddf63fa2e2a1a022b8ec7183
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-08-12 14:35:08 +0500

    Added create_superuser funtion to custom model

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model that supports using email instead of username"""
@@ -23,6 +32,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
-    objects = UserManager() # get_user_model().objects deganda yuqoridagi ishlaydi.
+    objects = UserManager()  # get_user_model().objects-yuqoridagi ishlaydi.
 
     USERNAME_FIELD = 'email'
